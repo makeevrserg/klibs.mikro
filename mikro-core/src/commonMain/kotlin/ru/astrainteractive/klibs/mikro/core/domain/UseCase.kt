bd067887f177efc59877f89485f9983282d4c0f8
Roman Makeev <makeevrserg@gmail.com>
2023-09-30 13:12:54 +0300

    fix: add operator to invoke function inside UseCase

--- mikro-core/src/commonMain/kotlin/ru/astrainteractive/klibs/mikro/core/domain/UseCase.kt
+++ mikro-core/src/commonMain/kotlin/ru/astrainteractive/klibs/mikro/core/domain/UseCase.kt
@@ -5,10 +5,10 @@ package ru.astrainteractive.klibs.mikro.core.domain
  */
 interface UseCase<out Output> {
     interface Parametrized<in Input, out Output> : UseCase<Output> {
-        suspend fun invoke(input: Input): Output
+        suspend operator fun invoke(input: Input): Output
     }
 
     interface Simple<out Output> : UseCase<Output> {
-        suspend fun invoke(): Output
+        suspend operator fun invoke(): Output
     }
 }
