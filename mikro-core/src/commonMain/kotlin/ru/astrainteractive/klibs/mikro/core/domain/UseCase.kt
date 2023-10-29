da9dcedc1f3c080b58abf179d2d044298dfe8b05
Roman Makeev <makeevrserg@gmail.com>
2023-10-29 20:30:38 +0300

    feat: suspended/blocking use case

--- mikro-core/src/commonMain/kotlin/ru/astrainteractive/klibs/mikro/core/domain/UseCase.kt
+++ mikro-core/src/commonMain/kotlin/ru/astrainteractive/klibs/mikro/core/domain/UseCase.kt
@@ -3,12 +3,26 @@ package ru.astrainteractive.klibs.mikro.core.domain
 /**
  * UseCase from CleanArchitecture
  */
-interface UseCase<out Output> {
-    interface Parametrized<in Input, out Output> : UseCase<Output> {
+interface UseCase<in Input, out Output> {
+    /**
+     * [Suspended] is use to call suspended methods
+     */
+    interface Suspended<in Input, out Output> : UseCase<Input, Output> {
         suspend operator fun invoke(input: Input): Output
+
+        interface Simple<out Output> : Suspended<Unit, Output> {
+            suspend operator fun invoke(): Output = invoke(Unit)
+        }
     }
 
-    interface Simple<out Output> : UseCase<Output> {
-        suspend operator fun invoke(): Output
+    /**
+     * [Blocking] is used to call blocking methods without coroutines
+     */
+    interface Blocking<in Input, out Output> : UseCase<Input, Output> {
+        operator fun invoke(input: Input): Output
+
+        interface Simple<out Output> : Blocking<Unit, Output> {
+            operator fun invoke(): Output = invoke(Unit)
+        }
     }
 }
