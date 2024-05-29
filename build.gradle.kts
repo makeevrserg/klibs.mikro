128526d2cbac03122161b9e78a1b1b789ee46223
makeevrserg <makeevrserg@gmail.com>
2024-05-29 23:52:38 +0300

    remove classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
-buildscript {
-    dependencies {
-        classpath(libs.makeevrserg.gradleplugin.convention)
-        classpath(libs.makeevrserg.gradleplugin.android)
-    }
-}
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.gradle.android) apply false
+    // klibs - core
+    alias(libs.plugins.klibs.gradle.detekt) apply false
+    alias(libs.plugins.klibs.gradle.detekt.compose) apply false
+    alias(libs.plugins.klibs.gradle.dokka.root) apply false
+    alias(libs.plugins.klibs.gradle.dokka.module) apply false
+    alias(libs.plugins.klibs.gradle.java.core) apply false
+    alias(libs.plugins.klibs.gradle.stub.javadoc) apply false
+    alias(libs.plugins.klibs.gradle.publication) apply false
+    alias(libs.plugins.klibs.gradle.rootinfo) apply false
+    // klibs - android
+    alias(libs.plugins.klibs.gradle.android.core) apply false
+    alias(libs.plugins.klibs.gradle.android.compose) apply false
+    alias(libs.plugins.klibs.gradle.android.apk.sign) apply false
+    alias(libs.plugins.klibs.gradle.android.apk.name) apply false
+    alias(libs.plugins.klibs.gradle.android.publication) apply false
 }
 
 apply(plugin = "ru.astrainteractive.gradleplugin.dokka.root")
