e4fcfafacc37ebb70373f6bdf731e95c93879362
Roman Makeev <makeevrserg@gmail.com>
2025-08-12 10:33:54 +0300

    fix await for completition

--- mikro-core/src/commonMain/kotlin/ru/astrainteractive/klibs/mikro/core/coroutines/SuspendExt.kt
+++ mikro-core/src/commonMain/kotlin/ru/astrainteractive/klibs/mikro/core/coroutines/SuspendExt.kt
@@ -1,9 +1,19 @@
 package ru.astrainteractive.klibs.mikro.core.coroutines
 
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.yield
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
-suspend fun awaitForCompletion(isCompleted: suspend () -> Boolean) {
-    while (!isCompleted.invoke()) {
-        yield()
+suspend fun awaitForCompletion(
+    delay: Duration = 50.milliseconds,
+    condition: suspend () -> Boolean
+) {
+    while (currentCoroutineContext().isActive && !condition.invoke()) {
+        val measure = measureTimeMillis { yield() }.milliseconds
+        if (measure < delay) delay(delay - measure)
     }
 }
