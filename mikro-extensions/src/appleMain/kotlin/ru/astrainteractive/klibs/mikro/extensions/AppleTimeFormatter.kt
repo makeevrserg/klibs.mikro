bcfd5f1c3ac7896b89741d8c2ac5032eb2dc3d18
Roman Makeev <makeevrserg@gmail.com>
2025-10-07 18:11:25 +0300

    fix build

--- mikro-extensions/src/appleMain/kotlin/ru/astrainteractive/klibs/mikro/extensions/AppleTimeFormatter.kt
+++ mikro-extensions/src/appleMain/kotlin/ru/astrainteractive/klibs/mikro/extensions/AppleTimeFormatter.kt
@@ -4,9 +4,11 @@ import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toNSDate
 import kotlinx.datetime.toNSTimeZone
+import kotlinx.datetime.toStdlibInstant
 import platform.Foundation.NSDateFormatter
 import platform.Foundation.NSLocale
 import platform.Foundation.currentLocale
+import kotlin.time.ExperimentalTime
 
 /**
  * Multiplatform ios/android time formatter
@@ -14,13 +16,14 @@ import platform.Foundation.currentLocale
 class AppleTimeFormatter(
     private val locale: NSLocale = NSLocale.currentLocale
 ) : TimeFormatter {
+    @OptIn(ExperimentalTime::class)
     override fun format(instant: Instant, format: String, timeZone: TimeZone): String {
         val dateFormatter = NSDateFormatter().apply {
             this.dateFormat = format
             this.timeZone = timeZone.toNSTimeZone()
             this.locale = this@AppleTimeFormatter.locale
         }
-        val nsDate = instant.toNSDate()
+        val nsDate = instant.toStdlibInstant().toNSDate()
         return dateFormatter.stringFromDate(nsDate)
     }
 }
